7e04dcadaf2072ed1dce8cbb07b4b4344100d4e9
burgerchisme <165308646+burgerchisme@users.noreply.github.com>
2024-04-04 19:34:39 -0400

    Add files via upload

---
+++ js/animaciones.js
@@ -0,0 +1,6 @@
+$(document).ready(function(){
+    $('#myCarousel').carousel({
+        interval: 3000, // Cambia cada 3 segundos
+        pause: "hover" // Pausa la animación al pasar el mouse sobre el carrusel
+    });
+});
